db808377635ee64474a3588370ecde59e9667895
timesleader <1449626263@qq.com>
2018-01-06 13:03:13 +0800

    Build adbd

--- omni_cheeseburger.mk
+++ omni_cheeseburger.mk
@@ -22,6 +22,9 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
+PRODUCT_PACKAGES += \
+    adbd
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_NAME := omni_cheeseburger
